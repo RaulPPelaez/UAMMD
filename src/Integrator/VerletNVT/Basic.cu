ff79242e881f4c7215d76a6f223514b65d5e5cd6
Raul P. Pelaez <raul.perez@uam.es>
2017-12-13 16:47:32 +0100

    Add some comments

--- src/Integrator/VerletNVT/Basic.cu
+++ src/Integrator/VerletNVT/Basic.cu
@@ -2,12 +2,9 @@
 
   This module integrates the dynamic of the particles using a two step velocity verlet MD algorithm
   that conserves the temperature, volume and number of particles.
+  
+  Uses a simple thermostat with velocity damping and a gaussian noise force.
 
-  For that several thermostats are (should be, currently only one) implemented:
-
-    -Velocity damping and gaussian noise 
-    - BBK ( TODO)
-    - SPV( TODO)
  Usage:
  
     Create the module as any other integrator with the following parameters:
@@ -17,14 +14,14 @@
     auto pd = make_shared<ParticleData>(N,sys);
     auto pg = make_shared<ParticleGroup>(pd,sys, "All");
     
-    
-    VerletNVT::Parameters par;
+    using NVT = VerletNVT::Basic;
+    NVT::Parameters par;
      par.temperature = 1.0;
      par.dt = 0.01;
      par.viscosity = 1.0;
      par.is2D = false;
 
-    auto verlet = make_shared<VerletNVT>(pd, pg, sys, par);
+    auto verlet = make_shared<NVT>(pd, pg, sys, par);
       
     //Add any interactor
     verlet->addInteractor(...);
@@ -34,10 +31,6 @@
     
     verlet->forwardTime();
     
-TODO:
-
-100- Outsource thermostat logic to a functor (external or internal)
-100-Implement thermostat from https://arxiv.org/pdf/1212.1244.pdf
  */
 
 #include"../VerletNVT.cuh"
