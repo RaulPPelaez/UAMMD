d2ac83bd34d3e1c9599765591bb86d31990ef6fe
Raul P. Pelaez <raul.perez@uam.es>
2018-02-13 20:44:32 +0100

    Add a PotentialUtils file containing an sfinae struct that will provide an Energy transverser even when a potential does not provide one (default is BasicNullTransverser)

---
+++ src/Interactor/Potential/PotentialUtils.cuh
@@ -0,0 +1,38 @@
+/*Raul P. Pelaez 2018. Some utilities for working with Potentials.
+
+  
+ */
+#ifndef POTENTIALUTILS_CUH
+#define POTENTIALUTILS_CUH
+
+#include"utils/cxx_utils.h"
+#include"misc/BasicNullTransverser.cuh"
+
+namespace uammd{
+  namespace Potential{
+    //This macro defines a struct called has_EnergyTransverser
+    SFINAE_DEFINE_HAS_MEMBER(EnergyTransverser)
+
+    //Calling get on this struct will provide the EnergyTransverser of a Potential if it exists, and will return a BasicNullTransverser otherwise
+    template<class T, bool hasEnergyTransverser = has_EnergyTransverser<T>::value>
+    struct getIfHasEnergyTransverser;
+
+    template<class T>
+    struct getIfHasEnergyTransverser<T, true>{
+      static decltype(&T::getEnergyTransverser) get(shared_ptr<T> t, Box box, shared_ptr<ParticleData> pd){
+	return t.getEnergyTransverser(box, pd);
+      }
+
+    };
+  
+    template<class T>
+    struct getIfHasEnergyTransverser<T, false>{
+      static BasicNullTransverser get(shared_ptr<T> t, Box box, shared_ptr<ParticleData> pd){
+	return BasicNullTransverser();
+      }
+
+    };
+  }
+}
+
+#endif
