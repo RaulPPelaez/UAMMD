34536611124796baa0be9ebfd4082ec56f732c84
Raul P. Pelaez <raul.perez@uam.es>
2019-11-22 17:19:28 +0100

    Add a core exception mechanism in utils/exception.h, defines uammd::exception as std::exception

---
+++ src/utils/exception.h
@@ -0,0 +1,29 @@
+#ifndef UAMMD_EXCEPTION_H
+#define UAMMD_EXCEPTION_H
+#include<exception>
+#include<cstring>
+#include<iostream>
+#include<stdexcept>
+#include"System/Log.h"
+
+namespace uammd{
+  using exception = std::exception;
+
+  void backtrace_nested_exception(const uammd::exception& e, int level =  0){
+
+    Logging::log<Logging::EXCEPTION>(std::string(level,' ') + "level " +
+				     std::to_string(level) + " exception: " + e.what());
+
+    try{
+      std::rethrow_if_nested(e);
+    }
+    catch(const std::exception& e){
+      backtrace_nested_exception(e, level+1);
+    }
+    catch(...){}
+
+  }
+
+};
+
+#endif
