509495170863800c505d9e75d20604bba8fc54d1
Raul P. Pelaez <raul.perez@uam.es>
2019-11-22 17:23:22 +0100

    Remove NVTX code from debugTools.cuh and modify error checking functions to throw exceptions at failure instead of exiting

--- src/utils/debugTools.cuh
+++ src/utils/debugTools.cuh
@@ -1,83 +1,57 @@
-/*Raul P. Pelaez 2017. Some utilities for debugging GPU code
+/*Raul P. Pelaez 2019. Some utilities for debugging GPU code
 
  */
 #ifndef DEBUGTOOLS_CUH
 #define DEBUGTOOLS_CUH
 
-
-//#define USE_NVTX
 #define CUDA_ERROR_CHECK
+
 #ifdef UAMMD_DEBUG
 #define CUDA_ERROR_CHECK_SYNC
 #endif
 
-#ifdef USE_NVTX
+#include"utils/exception.h"
 
+#define CudaSafeCall(err) __cudaSafeCall(err, __FILE__, __LINE__)
+#define CudaCheckError() __cudaCheckError(__FILE__, __LINE__)
 
-#include "nvToolsExt.h"
+namespace uammd{
 
-const uint32_t colors[] = { 0x0000ff00, 0x000000ff, 0x00ffff00, 0x00ff00ff, 0x0000ffff, 0x00ff0000, 0x00ffffff };
-const int num_colors = sizeof(colors)/sizeof(uint32_t);
+  class cuda_generic_error: public std::runtime_error{
+    cudaError_t error_code;
+  public:
+    cuda_generic_error(std::string msg, cudaError_t err):
+      std::runtime_error(msg + ": " + cudaGetErrorString(err) + " - code: " + std::to_string(err)),
+      error_code(err){}
 
-#define PUSH_RANGE(name,cid) { \
-    int color_id = cid; \
-    color_id = color_id%num_colors;\
-    nvtxEventAttributes_t eventAttrib = {0}; \
-    eventAttrib.version = NVTX_VERSION; \
-    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
-    eventAttrib.colorType = NVTX_COLOR_ARGB; \
-    eventAttrib.color = colors[color_id]; \
-    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
-    eventAttrib.message.ascii = name; \
-    nvtxRangePushEx(&eventAttrib); \
-  }
-#define POP_RANGE nvtxRangePop();
-#else
-#define PUSH_RANGE(name,cid)
-#define POP_RANGE
-#endif
+    cudaError_t code(){return error_code;}
+  };
 
-
-#define CudaSafeCall( err ) __cudaSafeCall( err, __FILE__, __LINE__ )
-#define CudaCheckError()    __cudaCheckError( __FILE__, __LINE__ )
-
-inline void __cudaSafeCall( cudaError err, const char *file, const int line )
-{
-  #ifdef CUDA_ERROR_CHECK
-  if ( cudaSuccess != err )
-    {
-      fprintf( stderr, "cudaSafeCall() failed at %s:%i : %s - code: %i\n",
-	       file, line, cudaGetErrorString( err ), err);
-      exit( -1 );
-    }
-  #endif
-
-  return;
 }
 
-inline void __cudaCheckError( const char *file, const int line )
-{
-
-  cudaError err = cudaGetLastError();
-  if ( cudaSuccess != err )
-    {
-      fprintf( stderr, "cudaCheckError() failed at %s:%i : %s\n",
-	       file, line, cudaGetErrorString( err ) );
-      exit( -1 );
-    }
-#ifdef CUDA_ERROR_CHECK_SYNC
-  // More careful checking. However, this will affect performance.
-  // Comment away if needed.
-  err = cudaDeviceSynchronize();
-  if( cudaSuccess != err )
-    {
-      fprintf( stderr, "cudaCheckError() with sync failed at %s:%i : %s\n",
-	       file, line, cudaGetErrorString( err ) );
-      exit( -1 );
-    }
+inline void __cudaSafeCall(cudaError err, const char *file, const int line){
+  #ifdef CUDA_ERROR_CHECK
+  if (cudaSuccess != err){
+    cudaGetLastError(); //Reset CUDA error status    
+    throw uammd::cuda_generic_error("CudaSafeCall() failed at "+
+				    std::string(file) + ":" + std::to_string(line), err);
+  }
   #endif
+}
 
-  return;
+inline void __cudaCheckError(const char *file, const int line){
+#ifdef CUDA_ERROR_CHECK_SYNC
+  err = cudaDeviceSynchronize();
+  if(cudaSuccess != err){
+    throw uammd::cuda_generic_error("CudaCheckError() with sync failed at "+
+				    std::string(file) + ":" + std::to_string(line), err);
+  }
+#endif
+  cudaError err = cudaGetLastError();
+  if(cudaSuccess != err){
+    throw uammd::cuda_generic_error("CudaCheckError() failed at "+
+				    std::string(file) + ":" + std::to_string(line), err);
+  }
 }
 
 #endif
