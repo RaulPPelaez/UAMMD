fc115aa595ec7ed9f6679ac63163046f80b8aaf7
RaulPPealez <raul.perez@uam.es>
2020-05-25 10:47:31 -0400

    Add BD::MidPoint Integrator

--- src/Integrator/BrownianDynamics.cu
+++ src/Integrator/BrownianDynamics.cu
@@ -1,17 +1,8 @@
-/*Raul P. Pelaez 2017. Brownian Euler Maruyama Integrator definition
-
-  Solves the following differential equation:
-      X[t+dt] = dt(K·X[t]+M·F[t]) + sqrt(2*T*dt)·dW·B
-   Being:
-     X - Positions
-     M - Self Mobility coefficient -> 1/(6·pi·vis·radius)
-     K - Shear matrix
-     dW- Noise vector
-     B - sqrt(M)
-*/
+/*Raul P. Pelaez 2017-2020. Brownian Dynamics Integrator definitions
+ */
 #include"BrownianDynamics.cuh"
 #include"third_party/saruprng.cuh"
-
+#include"utils/debugTools.h"
 namespace uammd{
   namespace BD{
 
@@ -33,8 +24,6 @@ namespace uammd{
       sys->log<System::MESSAGE>("[BD::EulerMaruyama] Initialized");
       int numberParticles = pg->getNumberParticles();
       this->selfMobility = 1.0/(6.0*M_PI*par.viscosity);
-      sys->log<System::MESSAGE>("[BD::EulerMaruyama] Temperature: %f", temperature);
-      sys->log<System::MESSAGE>("[BD::EulerMaruyama] dt: %f", dt);
       if(par.hydrodynamicRadius != real(-1.0)){
 	this->selfMobility /= par.hydrodynamicRadius;
 	this->hydrodynamicRadius = par.hydrodynamicRadius;
@@ -56,6 +45,8 @@ namespace uammd{
 	sys->log<System::MESSAGE>("[BD::EulerMaruyama] Hydrodynamic radius: %f", hydrodynamicRadius);
 	sys->log<System::MESSAGE>("[BD::EulerMaruyama] Self Mobility: %f", selfMobility);
       }
+      sys->log<System::MESSAGE>("[BD::EulerMaruyama] Temperature: %f", temperature);
+      sys->log<System::MESSAGE>("[BD::EulerMaruyama] dt: %f", dt);
       this->sqrt2MTdt = sqrt(2.0*selfMobility*temperature*dt);
       if(par.K.size()==3){
 	Kx = par.K[0];
@@ -69,22 +60,22 @@ namespace uammd{
       if(is2D){
 	sys->log<System::MESSAGE>("[BD::EulerMaruyama] Starting in 2D mode");
       }
-      cudaStreamCreate(&forceStream);
+      CudaSafeCall(cudaStreamCreate(&st));
     }
 
     EulerMaruyama::~EulerMaruyama(){
       sys->log<System::MESSAGE>("[BD::EulerMaruyama] Destroyed");
-      cudaStreamDestroy(forceStream);
+      cudaStreamDestroy(st);
     }
 
     namespace EulerMaruyama_ns{
-      /*Integrate the movement*/
-      __global__ void integrateGPU(real4* __restrict__  pos,
-				   ParticleGroup::IndexIterator __restrict__ indexIterator,
-				   const real4* __restrict__  force,
+
+      __global__ void integrateGPU(real4* pos,
+				   ParticleGroup::IndexIterator indexIterator,
+				   const real4* force,
 				   real3 Kx, real3 Ky, real3 Kz,
 				   real selfMobility,
-				   real* __restrict__ radius,
+				   const real* radius,
 				   real dt,
 				   bool is2D,
 				   real sqrt2MTdt,
@@ -97,7 +88,9 @@ namespace uammd{
 	real3 f = make_real3(force[i]);
 	real3 KR = make_real3(dot(Kx, p), dot(Ky, p), dot(Kz, p));
 	real invRadius = real(1.0);
-	if(radius) invRadius = real(1.0)/radius[i];
+	if(radius){
+	  invRadius = real(1.0)/radius[i];
+	}
 	// X[t+dt] = dt(K·X[t]+M·F[t]) + sqrt(2·T·dt)·dW·B
 	p += dt*( KR + selfMobility*invRadius*f);
 	if(sqrt2MTdt > real(0.0)){ //When temperature > 0
@@ -120,7 +113,14 @@ namespace uammd{
     void EulerMaruyama::forwardTime(){
       steps++;
       sys->log<System::DEBUG1>("[BD::EulerMaruyama] Performing integration step %d", steps);
-      int numberParticles = pg->getNumberParticles();
+      updateInteractors();
+      resetForces();
+      for(auto forceComp: interactors){
+	forceComp->sumForce(st);
+      }
+      updatePositions();
+    }
+    void EulerMaruyama::updateInteractors(){
       for(auto forceComp: interactors) forceComp->updateSimulationTime(steps*dt);
       if(steps==1){
 	for(auto forceComp: interactors){
@@ -128,37 +128,260 @@ namespace uammd{
 	  forceComp->updateTemperature(temperature);
 	}
       }
-      int BLOCKSIZE = 128;
-      uint Nthreads = BLOCKSIZE<numberParticles?BLOCKSIZE:numberParticles;
-      uint Nblocks = numberParticles/Nthreads +  ((numberParticles%Nthreads!=0)?1:0);
-      /*Compute new forces*/
-      auto groupIterator = pg->getIndexIterator(access::location::gpu);
-      {
-	auto force = pd->getForce(access::location::gpu, access::mode::write);
-	fillWithGPU<<<Nblocks, Nthreads>>>(force.raw(), groupIterator, make_real4(0), numberParticles);
-      }
-      for(auto forceComp: interactors) forceComp->sumForce(forceStream);
-      real * d_radius = nullptr;
+    }
+    void EulerMaruyama::resetForces(){
+      int numberParticles = pg->getNumberParticles();
+      auto force = pd->getForce(access::location::gpu, access::mode::write);
+      auto forceGroup = pg->getPropertyIterator(force);
+      thrust::fill(thrust::cuda::par.on(st), forceGroup, forceGroup + numberParticles, real4());
+    }
+
+    real* EulerMaruyama::getParticleRadiusIfAvailable(){
+      real* d_radius = nullptr;
       if(hydrodynamicRadius == real(-1.0) && pd->isRadiusAllocated()){
 	auto radius = pd->getRadius(access::location::gpu, access::mode::read);
 	d_radius = radius.raw();
 	sys->log<System::DEBUG3>("[BD::EulerMaruyama] Using particle radius.");
       }
+      return d_radius;
+    }
+
+    void EulerMaruyama::updatePositions(){
+      int numberParticles = pg->getNumberParticles();
+      int BLOCKSIZE = 128;
+      uint Nthreads = BLOCKSIZE<numberParticles?BLOCKSIZE:numberParticles;
+      uint Nblocks = numberParticles/Nthreads +  ((numberParticles%Nthreads!=0)?1:0);
+      real * d_radius = getParticleRadiusIfAvailable();
+      auto groupIterator = pg->getIndexIterator(access::location::gpu);
       auto pos = pd->getPos(access::location::gpu, access::mode::readwrite);
       auto force = pd->getForce(access::location::gpu, access::mode::read);
-      EulerMaruyama_ns::integrateGPU<<<Nblocks, Nthreads>>>(pos.raw(),
-							    groupIterator,
-							    force.raw(),
-							    Kx, Ky, Kz,
-							    selfMobility,
-							    d_radius,
-							    dt,
-							    is2D,
-							    sqrt2MTdt,
-							    numberParticles,
-							    steps, seed);
+      EulerMaruyama_ns::integrateGPU<<<Nblocks, Nthreads, 0, st>>>(pos.raw(),
+								   groupIterator,
+								   force.raw(),
+								   Kx, Ky, Kz,
+								   selfMobility,
+								   d_radius,
+								   dt,
+								   is2D,
+								   sqrt2MTdt,
+								   numberParticles,
+								   steps, seed);
 
     }
+
+
+
+
+
+
+    MidPoint::MidPoint(shared_ptr<ParticleData> pd,
+		       shared_ptr<ParticleGroup> pg,
+		       shared_ptr<System> sys,
+		       Parameters par):
+      Integrator(pd, pg, sys, "[BD::MidPoint]"),
+      Kx(make_real3(0)),
+      Ky(make_real3(0)),
+      Kz(make_real3(0)),
+      temperature(par.temperature),
+      dt(par.dt),
+      is2D(par.is2D),
+      steps(0){
+      sys->rng().next32();
+      sys->rng().next32();
+      seed = sys->rng().next32();
+      sys->log<System::MESSAGE>("[BD::MidPoint] Initialized");
+      int numberParticles = pg->getNumberParticles();
+      this->selfMobility = 1.0/(6.0*M_PI*par.viscosity);
+      if(par.hydrodynamicRadius != real(-1.0)){
+	this->selfMobility /= par.hydrodynamicRadius;
+	this->hydrodynamicRadius = par.hydrodynamicRadius;
+	if(pd->isRadiusAllocated()){
+	  sys->log<System::WARNING>("[BD::MidPoint] Assuming all particles have hydrodynamic radius %f",
+				    par.hydrodynamicRadius);
+	}
+	else{
+	  sys->log<System::MESSAGE>("[BD::MidPoint] Hydrodynamic radius: %f", par.hydrodynamicRadius);
+	}
+	sys->log<System::MESSAGE>("[BD::MidPoint] Self Mobility: %f", selfMobility);
+      }
+      else if(pd->isRadiusAllocated()){
+	sys->log<System::MESSAGE>("[BD::MidPoint] Hydrodynamic radius: particleRadius");
+	sys->log<System::MESSAGE>("[BD::MidPoint] Self Mobility: %f/particleRadius", selfMobility);
+      }
+      else{
+	this->hydrodynamicRadius = real(1.0);
+	sys->log<System::MESSAGE>("[BD::MidPoint] Hydrodynamic radius: %f", hydrodynamicRadius);
+	sys->log<System::MESSAGE>("[BD::MidPoint] Self Mobility: %f", selfMobility);
+      }
+      sys->log<System::MESSAGE>("[BD::MidPoint] Temperature: %f", temperature);
+      sys->log<System::MESSAGE>("[BD::MidPoint] dt: %f", dt);
+      if(par.K.size()==3){
+	Kx = par.K[0];
+	Ky = par.K[1];
+	Kz = par.K[2];
+	sys->log<System::MESSAGE>("[BD::MidPoint] Shear Matrix: [ %f %f %f; %f %f %f; %f %f %f ]",
+				  Kx.x, Kx.y, Kx.z,
+				  Ky.x, Ky.y, Ky.z,
+				  Kz.x, Kz.y, Kz.z);
+      }
+      if(is2D){
+	sys->log<System::MESSAGE>("[BD::MidPoint] Starting in 2D mode");
+      }
+      this->sqrtDdt = sqrt(selfMobility*temperature*dt);
+      CudaSafeCall(cudaStreamCreate(&st));
+    }
+
+    MidPoint::~MidPoint(){
+      sys->log<System::MESSAGE>("[BD::MidPoint] Destroyed");
+      cudaStreamDestroy(st);
+    }
+
+    namespace MidPoint_ns{
+
+      template<int step>
+      __global__ void integrateGPU(real4* pos,
+				   real3* initialPositions,
+				   ParticleGroup::IndexIterator indexIterator,
+				   const real4* force,
+				   real3 Kx, real3 Ky, real3 Kz,
+				   real selfMobility,
+				   const real* radius,
+				   real dt,
+				   bool is2D,
+				   real sqrt2MTdt,
+				   int N,
+				   uint stepNum, uint seed){
+        const auto id = blockIdx.x*blockDim.x+threadIdx.x;
+	if(id>=N) return;
+	const int i = indexIterator[id];
+	real3 p;
+	if(step==0){
+	  p = make_real3(pos[i]);
+	  initialPositions[id] = p;
+	}
+	else if(step==1){
+	  p = initialPositions[id];
+	}
+	real invRadius = real(1.0);
+	if(radius){
+	  invRadius = real(1.0)/radius[i];
+	}
+	real3 KR = make_real3(dot(Kx, p), dot(Ky, p), dot(Kz, p));
+	real3 f = make_real3(force[i]);
+	if(step == 0){
+	  f*= real(0.5);
+	  KR*=real(0.5);
+	}
+	p += dt*( KR + selfMobility*invRadius*f);
+	if(sqrt2MTdt > real(0.0)){ //When temperature > 0
+	  real sqrtInvRadius = real(1.0);
+	  if(radius){
+	    sqrtInvRadius = sqrtf(invRadius);
+	  }
+	  const real noiseAmplitude = sqrt2MTdt*sqrtInvRadius;
+	  Saru rng(id, stepNum, seed);
+	  real3 dW = make_real3(rng.gf(0, noiseAmplitude), 0);
+	  if(!is2D)   dW.z = rng.gf(0, noiseAmplitude).x;
+	  p += dW;
+	  if(step==1){
+	    dW = make_real3(rng.gf(0, noiseAmplitude), 0);
+	    if(!is2D)   dW.z = rng.gf(0, noiseAmplitude).x;
+	    p += dW;
+	  }
+	}
+	pos[i].x = p.x;
+	pos[i].y = p.y;
+	if(!is2D)
+	  pos[i].z = p.z;
+      }
+    }
+
+    void MidPoint::forwardTime(){
+      steps++;
+      sys->log<System::DEBUG1>("[BD::MidPoint] Performing integration step %d", steps);
+      updateInteractors();
+      computeCurrentForces();
+      updatePositionsFirstStep();
+      computeCurrentForces();
+      updatePositionsSecondStep();
+      CudaCheckError();
+    }
+
+    void MidPoint::computeCurrentForces(){
+      resetForces();
+      for(auto forceComp: interactors){
+	forceComp->sumForce(st);
+      }
+      CudaCheckError();
+    }
+
+    void MidPoint::updateInteractors(){
+      for(auto forceComp: interactors){
+	forceComp->updateSimulationTime(steps*dt);
+      }
+      if(steps==1){
+	for(auto forceComp: interactors){
+	  forceComp->updateTimeStep(dt);
+	  forceComp->updateTemperature(temperature);
+	}
+      }
+      CudaCheckError();
+    }
+
+    void MidPoint::resetForces(){
+      int numberParticles = pg->getNumberParticles();
+      auto force = pd->getForce(access::location::gpu, access::mode::write);
+      auto forceGroup = pg->getPropertyIterator(force);
+      thrust::fill(thrust::cuda::par.on(st), forceGroup, forceGroup + numberParticles, real4());
+      CudaCheckError();
+    }
+
+    real* MidPoint::getParticleRadiusIfAvailable(){
+      real* d_radius = nullptr;
+      if(hydrodynamicRadius == real(-1.0) and pd->isRadiusAllocated()){
+	auto radius = pd->getRadius(access::location::gpu, access::mode::read);
+	d_radius = radius.raw();
+	sys->log<System::DEBUG3>("[BD::MidPoint] Using particle radius.");
+      }
+      return d_radius;
+    }
+
+    void MidPoint::updatePositionsFirstStep(){
+      updatePositions<0>();
+    }
+
+    void MidPoint::updatePositionsSecondStep(){
+      updatePositions<1>();
+    }
+
+    template<int step>
+    void MidPoint::updatePositions(){
+      int numberParticles = pg->getNumberParticles();
+      int BLOCKSIZE = 128;
+      uint Nthreads = BLOCKSIZE<numberParticles?BLOCKSIZE:numberParticles;
+      uint Nblocks = numberParticles/Nthreads +  ((numberParticles%Nthreads!=0)?1:0);
+      real * d_radius = getParticleRadiusIfAvailable();
+      auto groupIterator = pg->getIndexIterator(access::location::gpu);
+      auto pos = pd->getPos(access::location::gpu, access::mode::readwrite);
+      auto force = pd->getForce(access::location::gpu, access::mode::read);
+      initialPositions.resize(numberParticles);
+      auto d_initialPositions = thrust::raw_pointer_cast(initialPositions.data());
+      MidPoint_ns::integrateGPU<step><<<Nblocks, Nthreads, 0, st>>>(pos.raw(),
+								    d_initialPositions,
+								    groupIterator,
+								    force.raw(),
+								    Kx, Ky, Kz,
+								    selfMobility,
+								    d_radius,
+								    dt,
+								    is2D,
+								    sqrtDdt,
+								    numberParticles,
+								    steps, seed);
+      CudaCheckError();
+    }
+
+
 
   }
 }
--- src/Integrator/BrownianDynamics.cuh
+++ src/Integrator/BrownianDynamics.cuh
@@ -1,4 +1,4 @@
-/*Raul P. Pelaez 2017. Brownian Euler Maruyama
+/*Raul P. Pelaez 2017-2020. Brownian Dynamics integrators
 
   Solves the following differential equation:
       X[t+dt] = dt(K·X[t]+M·F[t]) + sqrt(2*Tdt)·dW·B
@@ -10,72 +10,66 @@
      B - sqrt(M)
 
 
-
-
 OPTIONS:
 
-BD::EulerMaruyama::Parameters par;
-
+BD::Parameters par;
 par.K -> a std:vector<real3> of three elements, encoding a 3x3 shear Matrix. zero(3,3) by default.
-
 par.temperature -> System Temperature
-
 par.viscosity -> System Viscosity
-
 par.hydrodynamicRadius -> Particle radius (if all particles have the same radius). Set this variable if pd->radius has not been set or you want all particles to have the same diffusive radius and ignore pd->radius.
-
 par.dt -> Time step size.
-
 par.is2D -> Set to true if the system lives in 2D.
-
 
 USAGE:
 Use as any other Integrator.
 
   auto sys = make_shared<System>();
   auto pd = make_shared<ParticleData>(N, sys);
-
   ...
-Set initial state
+//Set initial state
   ...
-
   auto pg = make_shared<ParticleGroup>(pd, sys, "All");
-
-  BD::EulerMaruyama::Parameters par;
+  BD::Parameters par;
   par.temperature = std::stod(argv[7]); //For example
   par.viscosity = 1.0;
   par.hydrodynamicRadius = 1.0;
   par.dt = std::stod(argv[3]); //For example
+  using Scheme = BD::EulerMaruyama;
+//using Scheme = BD::MidPoint;
+  auto bd = make_shared<Scheme>(pd, pg, sys, par);
 
-  auto bd = make_shared<BD::EulerMaruyama>(pd, pg, sys, par);
+See examples/BD.cu for an example
 
-See exampleS/BD.cu for an example
+References:
 
+[1] Temporal Integrators for Fluctuating Hydrodynamics. Delong et. al. (2013) Phys. Rev. E 87, 033302.
 */
-#ifndef BROWNIANEULERMARUYAMAINTEGRATOR_CUH
-#define BROWNIANEULERMARUYAMAINTEGRATOR_CUH
+#ifndef BROWNIANDYNAMICSINTEGRATOR_CUH
+#define BROWNIANDYNAMICSINTEGRATOR_CUH
 #include"global/defines.h"
 #include"Integrator.cuh"
 
 namespace uammd{
   namespace BD{
+    struct Parameters{
+      //The 3x3 shear matrix is encoded as an array of 3 real3
+      std::vector<real3> K;
+      real temperature = 0;
+      real viscosity = 1;
+      real hydrodynamicRadius = -1.0;
+      real dt = 0;
+      bool is2D = false;
+    };
+
     class EulerMaruyama: public Integrator{
     public:
-      struct Parameters{
-	//The 3x3 shear matrix is encoded as an array of 3 real3
-	std::vector<real3> K;
-	real temperature = 0;
-	real viscosity = 1;
-	real hydrodynamicRadius = -1.0;
-	real dt = 0;
-	bool is2D = false;
-      };
+      using Parameters = BD::Parameters;
 
-      //Constructor, you have to provide D and K.
       EulerMaruyama(shared_ptr<ParticleData> pd,
 		    shared_ptr<ParticleGroup> pg,
 		    shared_ptr<System> sys,
 		    Parameters par);
+
       EulerMaruyama(shared_ptr<ParticleData> pd,
 		    shared_ptr<System> sys,
 		    Parameters par):
@@ -92,14 +86,58 @@ namespace uammd{
       real temperature = real(0.0);
       real sqrt2MTdt;
       real dt;
-
       bool is2D;
-
-      cudaStream_t forceStream;
-
+      cudaStream_t st;
       int steps;
       uint seed;
+
+      void updateInteractors();
+      void resetForces();
+      real* getParticleRadiusIfAvailable();
+      void updatePositions();
     };
+
+    //Implements the algorithm in [1]
+    class MidPoint: public Integrator{
+    public:
+      using Parameters = BD::Parameters;
+
+      MidPoint(shared_ptr<ParticleData> pd,
+	      shared_ptr<ParticleGroup> pg,
+	      shared_ptr<System> sys,
+	      Parameters par);
+
+      MidPoint(shared_ptr<ParticleData> pd,
+	      shared_ptr<System> sys,
+	      Parameters par):
+	MidPoint(pd, std::make_shared<ParticleGroup>(pd, sys), sys, par){}
+
+      ~MidPoint();
+
+      void forwardTime() override;
+
+    protected:
+      real3 Kx, Ky, Kz;
+      real selfMobility;
+      real hydrodynamicRadius = real(-1.0);
+      real temperature = real(0.0);
+      real sqrtDdt;
+      real dt;
+      bool is2D;
+      cudaStream_t st;
+      int steps;
+      uint seed;
+      thrust::device_vector<real3> initialPositions;
+      void updateInteractors();
+      void resetForces();
+      void computeCurrentForces();
+      real* getParticleRadiusIfAvailable();
+      void updatePositionsFirstStep();
+      void updatePositionsSecondStep();
+      template<int step> void updatePositions();
+
+    };
+
   }
 }
 
