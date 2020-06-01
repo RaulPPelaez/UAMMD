2b9114efbfe0c4f0b6bac52af7524ff51c2c5c68
RaulPPealez <raul.perez@uam.es>
2020-06-01 05:06:17 -0400

    Update BD test script

--- test/BD/BD.cu
+++ test/BD/BD.cu
@@ -1,10 +1,6 @@
 /*Raul P. Pelaez 2020. BD  test code.
-
-  Runs a simulation with particles interacting with LJ.
-
     Requires a data.main with the parameters (see the readParameters function).
-
-  Intended to be used by test.bash
+    Intended to be used by test.bash
  */
 #include "uammd.cuh"
 #include "Integrator/BrownianDynamics.cuh"
@@ -13,11 +9,12 @@
 #include"Interactor/NeighbourList/CellList.cuh"
 #include"Interactor/NeighbourList/VerletList.cuh"
 #include"Interactor/PairForces.cuh"
+#include"Interactor/IBM_Poisson.cuh"
 #include"Interactor/Potential/Potential.cuh"
 #include"SoftPotential.cuh"
+#include"RepulsivePotential.cuh"
 
 using namespace uammd;
-
 
 struct Parameters{
   real3 boxSize;
@@ -30,28 +27,47 @@ struct Parameters{
   real temperature;
   real viscosity;
   real hydrodynamicRadius;
+  real tolerance;
   std::string outfile;
   std::string scheme;
   std::string potential;
+  std::string readFile;
+  bool noInteractors = false;
+  //Poisson
+  bool useElectro = false;
+  real gw,permitivity, split;
   //LJ
   real sigma;
   real epsilon;
   bool shift;
   //Soft
   real U0, d, b;
+  //Repulsive
+  real r_m;
+  int p;
 
 };
 
 struct UAMMD{
+  std::shared_ptr<System> sys;
   std::shared_ptr<ParticleData> pd;
   std::shared_ptr<ParticleGroup> pg;
-  std::shared_ptr<System> sys;
   Parameters parameters;
 };
 
 UAMMD initialize(int argc, char* argv[]);
 
 std::shared_ptr<Integrator> createIntegrator(UAMMD sim);
+
+std::shared_ptr<Poisson> createElectrostaticInteractor(UAMMD sim){
+  Poisson::Parameters par;
+  par.box = Box(sim.parameters.boxSize);
+  par.epsilon = sim.parameters.permitivity;
+  par.tolerance = sim.parameters.tolerance;
+  par.gw = sim.parameters.gw;
+  par.split = sim.parameters.split;
+  return std::make_shared<Poisson>(sim.pd, sim.pg, sim.sys, par);
+}
 
 std::shared_ptr<Interactor> createShortRangeInteractor(UAMMD sim);
 
@@ -61,12 +77,18 @@ int main(int argc, char *argv[]){
   auto sim = initialize(argc, argv);
   std::shared_ptr<Integrator> mc;
   mc = createIntegrator(sim);
-  mc->addInteractor(createShortRangeInteractor(sim));
+  if(not sim.parameters.noInteractors){
+    mc->addInteractor(createShortRangeInteractor(sim));
+    if(sim.parameters.useElectro){
+      mc->addInteractor(createElectrostaticInteractor(sim));
+    }
+  }
   Timer tim; tim.tic();
   runSimulation(sim, mc);
   auto totalTime = tim.toc();
   sim.sys->log<System::MESSAGE>("mean FPS: %.2f", sim.parameters.numberSteps/totalTime);
   sim.sys->finish();
+  CudaCheckError();
   return 0;
 }
 
@@ -76,6 +98,27 @@ void initializePositions(UAMMD sim){
   auto pos = sim.pd->getPos(access::location::cpu, access::mode::write);
   auto initial =  initLattice(sim.parameters.boxSize, sim.parameters.numberParticles, sc);
   std::copy(initial.begin(), initial.end(), pos.begin());
+  std::generate(pos.begin(), pos.end(),
+		[&](){return make_real4(make_real3(sim.sys->rng().uniform3(-0.5, 0.5))*sim.parameters.boxSize, 0);});
+}
+void initializeCharges(UAMMD sim){
+  auto charges = sim.pd->getCharge(access::location::cpu, access::mode::write);
+  int i = 0;
+  int N = sim.parameters.numberParticles;
+  std::generate(charges.begin(), charges.end(), [i,N]() mutable { return pow(-1, ++i);});
+  std::random_device rd;
+  std::mt19937 g(rd());
+  std::shuffle(charges.begin(), charges.end(), g);
+}
+
+void initializeFromFile(UAMMD sim){
+  auto pos = sim.pd->getPos(access::location::cpu, access::mode::write);
+  auto charge = sim.pd->getCharge(access::location::cpu, access::mode::write);
+  std::ifstream in(sim.parameters.readFile);
+  fori(0, sim.parameters.numberParticles){
+    in>>pos[i].x>>pos[i].y>>pos[i].z>>charge[i];
+    pos[i].w = 0;
+  }
 }
 
 UAMMD initialize(int argc, char* argv[]){
@@ -83,9 +126,19 @@ UAMMD initialize(int argc, char* argv[]){
   sim.sys = std::make_shared<System>(argc, argv);
   ullint seed = 0xf31337Bada55D00dULL^time(NULL);
   sim.sys->rng().setSeed(seed);
-  sim.parameters = readParameters("data.main", sim.sys);
+  std::string fileName = "data.main";
+  if(argc>1){
+    fileName = argv[1];
+  }
+  sim.parameters = readParameters(fileName, sim.sys);
   sim.pd = std::make_shared<ParticleData>(sim.parameters.numberParticles, sim.sys);
-  initializePositions(sim);
+  if(!sim.parameters.readFile.empty()){
+    initializeFromFile(sim);
+  }
+  else{
+    initializePositions(sim);
+    initializeCharges(sim);
+  }
   sim.pg = std::make_shared<ParticleGroup>(sim.pd, sim.sys, "All");
   return sim;
 }
@@ -107,12 +160,18 @@ std::shared_ptr<Integrator> createIntegrator(UAMMD sim){
   else if(sim.parameters.scheme.compare("MidPoint") == 0){
     return createIntegratorBD<BD::MidPoint>(sim);
   }
+  else if(sim.parameters.scheme.compare("AdamsBashforth") == 0){
+    return createIntegratorBD<BD::AdamsBashforth>(sim);
+  }
+  else if(sim.parameters.scheme.compare("Leimkuhler") == 0){
+    return createIntegratorBD<BD::Leimkuhler>(sim);
+  }
   else{
-    sim.sys->log<System::CRITICAL>("Unrecognized scheme, use EulerMaruyama pr MidPoint");
+    sim.sys->log<System::CRITICAL>("Unrecognized scheme, use EulerMaruyama, MidPoint, AdamsBashforth or Leimkuhler");
     exit(1);
     return nullptr;
   }
-    
+
 }
 
 
@@ -141,6 +200,19 @@ template<> std::shared_ptr<SoftPotential> createPotential(UAMMD sim){
  return pot;
 }
 
+template<> std::shared_ptr<RepulsivePotential> createPotential(UAMMD sim){
+  auto pot = std::make_shared<RepulsivePotential>(sim.sys);
+  RepulsivePotential::InputPairParameters ppar;
+  ppar.cutOff = sim.parameters.cutOff;
+  ppar.U0 = sim.parameters.U0;
+  ppar.sigma = sim.parameters.sigma;
+  ppar.r_m = sim.parameters.r_m;
+  ppar.p = sim.parameters.p;
+  sim.sys->log<System::MESSAGE>("Repulsive rcut: %g", ppar.cutOff);
+  pot->setPotParameters(0, 0, ppar);
+ return pot;
+}
+
 
 template<class UsePotential> std::shared_ptr<Interactor> createShortRangeInteractor_impl(UAMMD sim){
   auto pot = createPotential<UsePotential>(sim);
@@ -158,8 +230,12 @@ std::shared_ptr<Interactor> createShortRangeInteractor(UAMMD sim){
   else if(sim.parameters.potential.compare("soft") == 0){
     return createShortRangeInteractor_impl<SoftPotential>(sim);
   }
+  else if(sim.parameters.potential.compare("repulsive") == 0){
+    return createShortRangeInteractor_impl<RepulsivePotential>(sim);
+  }
+
   else{
-    System::log<System::CRITICAL>("Unrecognized potential. Use LJ or soft");
+    System::log<System::CRITICAL>("Unrecognized potential. Use LJ, soft or repulsive");
     return nullptr;
   }
 }
@@ -181,13 +257,18 @@ public:
     sim.sys->log<System::DEBUG1>("[System] Writing to disk...");
     auto par = sim.parameters;
     auto pos = sim.pd->getPos(access::location::cpu, access::mode::read);
+    auto charge = sim.pd->getCharge(access::location::cpu, access::mode::read);
     const int * sortedIndex = sim.pd->getIdOrderedIndices(access::location::cpu);
     out<<"#Lx="<<0.5*par.boxSize.x<<";Ly="<<0.5*par.boxSize.y<<";Lz="<<0.5*par.boxSize.z<<";"<<std::endl;
     real3 p;
     fori(0, par.numberParticles){
       real4 pc = pos[sortedIndex[i]];
       p = make_real3(pc);
-      out<<p<<" 0.5 0\n";
+      int type = 0;
+      if(sim.parameters.useElectro){
+	type = (charge[sortedIndex[i]]+1)*0.5;
+      }
+      out<<p<<" 0.5 "<<type<<"\n";
     }
     out<<std::flush;
   }
@@ -221,21 +302,40 @@ Parameters readParameters(std::string datamain, shared_ptr<System> sys){
   in.getOption("viscosity", InputFile::Required)>>par.viscosity;
   in.getOption("hydrodynamicRadius", InputFile::Required)>>par.hydrodynamicRadius;
   in.getOption("outfile", InputFile::Required)>>par.outfile;
-  in.getOption("cutOff", InputFile::Required)>>par.cutOff;
   in.getOption("potential", InputFile::Required)>>par.potential;
   if(par.potential.compare("LJ") == 0){
     in.getOption("sigma", InputFile::Required)>>par.sigma;
     in.getOption("epsilon", InputFile::Required)>>par.epsilon;
     in.getOption("shiftLJ", InputFile::Required)>>par.shift;
+    in.getOption("cutOff", InputFile::Required)>>par.cutOff;
   }
   else if(par.potential.compare("soft") == 0){
     in.getOption("U0", InputFile::Required)>>par.U0;
     in.getOption("b", InputFile::Required)>>par.b;
     in.getOption("d", InputFile::Required)>>par.d;
+    in.getOption("cutOff", InputFile::Required)>>par.cutOff;
+  }
+  else if(par.potential.compare("repulsive") == 0){
+    in.getOption("U0", InputFile::Required)>>par.U0;
+    in.getOption("r_m", InputFile::Required)>>par.r_m;
+    in.getOption("p", InputFile::Required)>>par.p;
+    in.getOption("sigma", InputFile::Required)>>par.sigma;
+    in.getOption("cutOff", InputFile::Required)>>par.cutOff;
+  }
+  else if(par.potential.compare("none") == 0){
+    par.noInteractors = true;
   }
 
   in.getOption("scheme", InputFile::Required)>>par.scheme;
-  
+  in.getOption("readFile", InputFile::Optional)>>par.readFile;
+  if(in.getOption("useElectrostatics")){
+    par.useElectro = true;
+    in.getOption("gw", InputFile::Required)>>par.gw;
+    in.getOption("tolerance", InputFile::Required)>>par.tolerance;
+    in.getOption("permitivity", InputFile::Required)>>par.permitivity;
+    in.getOption("split", InputFile::Required)>>par.split;
+  }
+
   return par;
 }
 
--- test/BD/test.bash
+++ test/BD/test.bash
@@ -10,8 +10,7 @@ then
    rm -rf MeanSquareDisplacement
 fi
 cd ..
-#scheme=EulerMaruyama
-scheme=MidPoint
+
 L=32
 N=16384
 vis=$(echo 1 | awk '{pi=atan2(0,-1); printf("%.14g\n", 1/(6.0*pi))}')
@@ -22,11 +21,14 @@ D0=$(echo $T $vis $rh | awk '{pi=atan2(0,-1);printf("%.14g\n",$1/(6*pi*$2*$3))}'
 relaxSteps=-1
 nsteps=1000
 printSteps=1
-dt=1e-6
-
-cat<<EOF > data.main
+dt=1
+make
+for scheme in AdamsBashforth MidPoint EulerMaruyama
+do
+    echo "Doing $scheme"
+    cat<<EOF > data.main
 scheme $scheme
-potential LJ
+potential none
 boxSize $L $L $L
 numberSteps $nsteps
 printSteps $printSteps
@@ -37,18 +39,12 @@ temperature    $T
 viscosity   $vis
 hydrodynamicRadius $rh
 outfile /dev/stdout
-cutOff  2.5
-sigma   1
-epsilon 1
-shiftLJ 0
 EOF
-
-make
-Nsteps=$(echo $nsteps $printSteps | awk '{print int($1/$2)}')
-./bd data.main 2>log |
-    tools/msd -N $N -Nsteps $Nsteps |
-    head -5 |
-    awk '{
+    Nsteps=$(echo $nsteps $printSteps | awk '{print int($1/$2)}')
+    ./bd data.main 2>log |
+	tools/msd -N $N -Nsteps $Nsteps |
+	head -5 |
+	awk '{
 	t+=$1;
 	t2+=$1*$1;
 	nf=NF;
@@ -58,13 +54,14 @@ Nsteps=$(echo $nsteps $printSteps | awk '{print int($1/$2)}')
 	}
 	}
 	END{
+	printf("Relative deviation from expected self Diffusion coefficient: ");
 	for(i=2; i<=nf; i++){
           m=(NR*xt[i] - x[i]*t)/(NR*t2-t*t);
-	  printf("Relative deviation from expected self Diffusion coefficient: %.14g ", 1.0-m/(2*'$dt'*'$printSteps'*'$D0'));
+	  printf("%.14g ", 1.0-m/(2*'$dt'*'$printSteps'*'$D0'));
 	}
 	printf("\n");}'
 
-
+done
 
 
 
