09acf8ab0c282c96f1c367b7628828257859dd38
RaulPPealez <raul.perez@uam.es>
2021-08-24 19:50:45 +0200

    Use the same cached container in FCM and DPStokes

--- src/Integrator/BDHI/DoublyPeriodic/StokesSlab/utils.cuh
+++ src/Integrator/BDHI/DoublyPeriodic/StokesSlab/utils.cuh
@@ -9,78 +9,12 @@
 #include "utils/cufftPrecisionAgnostic.h"
 #include "utils/cufftComplex4.cuh"
 #include "utils/cufftComplex2.cuh"
-
+#include"utils/container.h"
 namespace uammd{
   namespace DPStokesSlab_ns{  
-    namespace detail{
-      //This is a very barebones container. Its purpose is only to avoid the unnecessary unninitialized_fill kernel that thrust issues on device_vector creation. Thus it mascarades as a thrust::device_vector.
-      template<class T, class Allocator = System::allocator<T>>
-      class UninitializedCachedContainer{
-	using Container = std::shared_ptr<T>;
-	using Ptr = T*;
-	using iterator = typename thrust::device_vector<T>::iterator;
-	Container m_data;
-	size_t m_size, capacity;
-
-	Container create(size_t s){
-	  if(s>0){
-	    try{
-	      return Container(Allocator().allocate(s), [](T* ptr){Allocator().deallocate(ptr);});
-	    }
-	    catch(...){
-	      System::log<System::EXCEPTION>("[UninitializedCachedContainer] Could not allocate buffer of size %zu", s);
-	      throw;
-	    }
-	  }
-	  else{
-	    return Container();
-	  }
-	}
-
-      public:
-	UninitializedCachedContainer(size_t i_size = 0)
-	  : m_size(0), capacity(0), m_data() {
-	  this->resize(i_size);
-	}
-
-	UninitializedCachedContainer(const std::vector<T> &other):
-	  UninitializedCachedContainer(other.size()){
-	  thrust::copy(other.begin(), other.end(), thrust::device_ptr<T>(begin()));
-	}
-
-	iterator begin() const{ return iterator(data()); }
-
-	iterator end() const{ return begin() + m_size; }
-
-	size_t size() const{
-	  return m_size;
-	}
-
-	void resize(size_t newsize){
-	  if(newsize > capacity){
-	    auto data2 = create(newsize);
-	    if(size()>0){
-	      thrust::copy(thrust::cuda::par, begin(), end(), data2.get());
-	    }
-	    m_data.swap(data2);
-	    capacity = newsize;
-	    m_size = newsize;
-	  }
-	  else{
-	    m_size = newsize;
-	  }
-	}
-
-	thrust::device_ptr<T> data() const{
-	  return thrust::device_ptr<T>(m_data.get());
-	}
-
-      };
-    }
 #ifndef UAMMD_DEBUG
     template<class T> using gpu_container = thrust::device_vector<T>;
-    //template<class T>  using cached_vector = thrust::device_vector<T, System::allocator_thrust<T>>;
-    template<class T>  using cached_vector = detail::UninitializedCachedContainer<T>;
+    template<class T>  using cached_vector = uninitialized_cached_vector<T>;
 #else
     template<class T> using gpu_container = thrust::device_vector<T, managed_allocator<T>>;
     template<class T> using cached_vector = thrust::device_vector<T, managed_allocator<T>>;
