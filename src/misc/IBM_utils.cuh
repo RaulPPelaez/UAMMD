09cfc3a0c5cff4503ce72ec3acc484d2b2a41f89
RaulPPealez <raul.perez@uam.es>
2022-09-16 15:21:06 +0200

    IBM: Do not assume the return type of the kernel function is real

--- src/misc/IBM_utils.cuh
+++ src/misc/IBM_utils.cuh
@@ -26,8 +26,8 @@ namespace uammd{
       SFINAE_DEFINE_HAS_MEMBER(phiX)
       SFINAE_DEFINE_HAS_MEMBER(phiY)
       SFINAE_DEFINE_HAS_MEMBER(phiZ)
-#define ENABLE_PHI_IF_HAS(foo) template<class Kernel> __device__ inline SFINAE::enable_if_t<has_phi##foo<Kernel>::value, real>
-#define ENABLE_PHI_IF_NOT_HAS(foo) template<class Kernel> __device__ inline SFINAE::enable_if_t<not has_phi##foo<Kernel>::value, real>
+#define ENABLE_PHI_IF_HAS(foo) template<class Kernel> __device__ inline SFINAE::enable_if_t<has_phi##foo<Kernel>::value, decltype(std::declval<Kernel>().phiX(real(), real3()))>
+#define ENABLE_PHI_IF_NOT_HAS(foo) template<class Kernel> __device__ inline SFINAE::enable_if_t<not has_phi##foo<Kernel>::value, decltype(std::declval<Kernel>().phi(real(), real3()))>
       ENABLE_PHI_IF_HAS(X) phiX(Kernel &kern, real r, real3 pos){return kern.phiX(r, pos);}
       ENABLE_PHI_IF_HAS(Y) phiY(Kernel &kern, real r, real3 pos){return kern.phiY(r, pos);}
       ENABLE_PHI_IF_HAS(Z) phiZ(Kernel &kern, real r, real3 pos){return kern.phiZ(r, pos);}
